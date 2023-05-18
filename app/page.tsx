a890bf72aa3d931943bbdbe765fcca9d7e722873
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-05-18 13:25:27 +0530

    changed quests to questions back

--- app/page.tsx
+++ app/page.tsx
@@ -38,7 +38,7 @@ export default function Home() {
         <div className="mx-auto flex max-w-2xl gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:items-start lg:gap-y-10">
           <div className="lg:mx-auto lg:flex lg:w-full lg:max-w-7xl lg:gap-x-8 lg:px-8">
             <h1 className="text-[40px] lg:text-5xl lg:leading-[56px] font-satoshiBlack capitalize bg-clip-text text-transparent bg-gradient-to-r from-slate-900 to-gray-600 text-center">
-              Frequently asked quests from Codewithantonio&apos;s videos.
+              Frequently asked questions from Codewithantonio&apos;s videos.
             </h1>
           </div>
         </div>
