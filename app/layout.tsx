51d27e544b68b862453c2d5ff784be01fdda0271
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-05-20 13:01:11 +0530

    added og-image

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,61 @@ import { Inter } from "next/font/google";
 
 import ClientOnly from "./components/ClientOnly";
 import "./globals.css";
+import { siteConfig } from "@/config/site";
 
 const inter = Inter({ subsets: ["latin"] });
 
+// export const metadata = {
+//   title: "Faq - CodewithAntonio",
+//   description: "Frequently asked questions based on the youtube videos.",
+// };
+
 export const metadata = {
-  title: "Faq - CodewithAntonio",
-  description: "Frequently asked questions based on the youtube videos.",
-};
+  title: {
+    default: siteConfig.name,
+    template: `%s | ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  keywords: [
+    "Next.js",
+    "React",
+    "Tailwind CSS",
+    "Server Components",
+    "Radix UI",
+  ],
+  authors: [
+    {
+      name: "shadcn",
+      url: "https://shadcn.com",
+    },
+  ],
+  creator: "shadcn",
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: [`${siteConfig.url}/og.jpg`],
+    creator: "@shadcn",
+  },
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: `${siteConfig.url}/site.webmanifest`,
+}
 
 export default function RootLayout({
   children,
