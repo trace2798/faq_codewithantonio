ab0e5c7c06f25c16324a8fa5565c340fad1d25b8
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-05-20 12:42:41 +0530

    removed env

--- .contentlayer/generated/Guide/_index.mjs
+++ .contentlayer/generated/Guide/_index.mjs
@@ -2,5 +2,6 @@
 
 import guides__buildBlogUsingContentlayerMdxMdx from './guides__build-blog-using-contentlayer-mdx.mdx.json' assert { type: 'json' }
 import guides__usingNextAuthNext_13Mdx from './guides__using-next-auth-next-13.mdx.json' assert { type: 'json' }
+import guides__usingTailwindCssNext_13Mdx from './guides__using-tailwind-css-next-13.mdx.json' assert { type: 'json' }
 
-export const allGuides = [guides__buildBlogUsingContentlayerMdxMdx, guides__usingNextAuthNext_13Mdx]
+export const allGuides = [guides__buildBlogUsingContentlayerMdxMdx, guides__usingNextAuthNext_13Mdx, guides__usingTailwindCssNext_13Mdx]
--- env.mjs
+++ env.mjs
@@ -1,5 +1,5 @@
-import { createEnv } from "@t3-oss/env-nextjs"
-import { z } from "zod"
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
 
 export const env = createEnv({
   server: {
@@ -10,14 +10,6 @@ export const env = createEnv({
     GITHUB_CLIENT_ID: z.string().min(1),
     GITHUB_CLIENT_SECRET: z.string().min(1),
     GITHUB_ACCESS_TOKEN: z.string().min(1),
-    DATABASE_URL: z.string().min(1),
-    SMTP_FROM: z.string().min(1),
-    POSTMARK_API_TOKEN: z.string().min(1),
-    POSTMARK_SIGN_IN_TEMPLATE: z.string().min(1),
-    POSTMARK_ACTIVATION_TEMPLATE: z.string().min(1),
-    STRIPE_API_KEY: z.string().min(1),
-    STRIPE_WEBHOOK_SECRET: z.string().min(1),
-    STRIPE_PRO_MONTHLY_PLAN_ID: z.string().min(1),
   },
   client: {
     NEXT_PUBLIC_APP_URL: z.string().min(1),
@@ -28,14 +20,6 @@ export const env = createEnv({
     GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
     GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
     GITHUB_ACCESS_TOKEN: process.env.GITHUB_ACCESS_TOKEN,
-    DATABASE_URL: process.env.DATABASE_URL,
-    SMTP_FROM: process.env.SMTP_FROM,
-    POSTMARK_API_TOKEN: process.env.POSTMARK_API_TOKEN,
-    POSTMARK_SIGN_IN_TEMPLATE: process.env.POSTMARK_SIGN_IN_TEMPLATE,
-    POSTMARK_ACTIVATION_TEMPLATE: process.env.POSTMARK_ACTIVATION_TEMPLATE,
-    STRIPE_API_KEY: process.env.STRIPE_API_KEY,
-    STRIPE_WEBHOOK_SECRET: process.env.STRIPE_WEBHOOK_SECRET,
-    STRIPE_PRO_MONTHLY_PLAN_ID: process.env.STRIPE_PRO_MONTHLY_PLAN_ID,
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
   },
-})
+});
