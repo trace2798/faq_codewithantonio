01ead52f262ff5aeb44a984b1528e319be5b7d7d
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-05-22 14:29:07 +0530

    changed favicon

--- app/guides/page.tsx
+++ app/guides/page.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 import { DocsPageHeader } from "../components/page-header";
 
 export const metadata = {
-  title: "Guides",
+  title: "faq CWA - Guides",
   description:
     "This section includes end-to-end guides for developing Next.js 13 apps.",
 };
