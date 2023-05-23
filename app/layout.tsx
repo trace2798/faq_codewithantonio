087c78a93c5b5bc1395134f0e7e881cb442c8748
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-05-23 17:39:24 +0530

    slight change

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import Script from "next/script";
 
 import ClientOnly from "./components/ClientOnly";
 import Providers from "./components/Providers";
@@ -69,6 +70,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Script
+        id="google-analytics"
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_CODE}`}
+      />
+
+      <Script id="google-analytics" strategy="lazyOnload">
+        {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_CODE}, {
+        page_path: window.location.pathname,
+      });
+  `}
+      </Script>
       <body className={inter.className}>
         <ClientOnly>
           <Providers>
