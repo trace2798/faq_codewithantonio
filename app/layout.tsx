f75ff302a24b21527d393cf9d59f2bf399820f36
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-05-23 17:52:09 +0530

    added keywords on main layout meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,54 +14,14 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Faq - CodewithAntonio",
   description: "Frequently asked questions based on the youtube videos.",
+  keywords: [
+    "Next.js",
+    "React",
+    "Tailwind CSS",
+    "CodewithAntonio",
+    "Next.js tutorials",
+  ],
 };
-
-// export const metadata = {
-//   title: {
-//     default: siteConfig.name,
-//     template: `%s | ${siteConfig.name}`,
-//   },
-//   description: siteConfig.description,
-//   keywords: [
-//     "Next.js",
-//     "React",
-//     "Tailwind CSS",
-//     "Server Components",
-//     "Radix UI",
-//   ],
-//   authors: [
-//     {
-//       name: "shadcn",
-//       url: "https://shadcn.com",
-//     },
-//   ],
-//   creator: "shadcn",
-//   themeColor: [
-//     { media: "(prefers-color-scheme: light)", color: "white" },
-//     { media: "(prefers-color-scheme: dark)", color: "black" },
-//   ],
-//   openGraph: {
-//     type: "website",
-//     locale: "en_US",
-//     url: siteConfig.url,
-//     title: siteConfig.name,
-//     description: siteConfig.description,
-//     siteName: siteConfig.name,
-//   },
-//   twitter: {
-//     card: "summary_large_image",
-//     title: siteConfig.name,
-//     description: siteConfig.description,
-//     images: [`${siteConfig.url}/og.jpg`],
-//     creator: "@shadcn",
-//   },
-//   icons: {
-//     icon: "/favicon.ico",
-//     shortcut: "/favicon-16x16.png",
-//     apple: "/apple-touch-icon.png",
-//   },
-//   manifest: `${siteConfig.url}/site.webmanifest`,
-// }
 
 export default function RootLayout({
   children,
