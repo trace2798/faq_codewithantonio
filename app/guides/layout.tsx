50cad3b1db5b3bb460b9c91243146464f06404d1
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-05-24 17:16:03 +0530

    added twitter og image meta on guide layout

--- app/guides/layout.tsx
+++ app/guides/layout.tsx
@@ -3,5 +3,22 @@ interface GuidesLayoutProps {
 }
 
 export default function GuidesLayout({ children }: GuidesLayoutProps) {
-  return <div className="mx-auto max-w-5xl max-lg:mx-[6vw]">{children}</div>;
+  return (
+    <>
+     <head>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@Tisonthemove247" />
+        <meta name="twitter:title" content="Guides for NextJs tutorials" />
+        <meta
+          name="twitter:description"
+          content="Guides based on youtube video topics."
+        />
+        <meta
+          name="twitter:image"
+          content="https://faq-codewithantonio.vercel.app/guide.jpg"
+        />
+      </head>
+      <div className="mx-auto max-w-5xl max-lg:mx-[6vw]">{children}</div>
+    </>
+  );
 }
