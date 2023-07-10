dad1fae402c95e87e84bba034c155c6f62eba5c3
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-07-10 22:40:59 +0530

    fixed some typo

--- app/components/accordion/data.tsx
+++ app/components/accordion/data.tsx
@@ -3,11 +3,11 @@ export const accordianData = [
     title: `The provided database string is invalid.`,
     description: `Add "test" in the end, `,
     description2: `For example, Before "mongodb+srv://project:<password>@cluster0.zwdoshb.mongodb.net/" After:"mongodb+srv://project:<password>@cluster0.zwccdhb.mongodb.net/test"`,
-    description3: `If you all ready have text then check your password.`,
+    description3: `If you already have test then check your password.`,
   },
   {
     title: "Build fail",
-    description: `Go to your package,json and then add "postbuild": "prisma generate" to your scripts.`,
+    description: `Go to your package,json and then add "postinstall": "prisma generate" to your scripts.`,
     description2: `Previously it was not required.`,
   },
   {
