d143f65ee032380702a9c68f75fc4d9ebb2bca24
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-07-11 23:27:21 +0530

    added data in accordion.

--- app/components/accordion/data.tsx
+++ app/components/accordion/data.tsx
@@ -15,4 +15,9 @@ export const accordianData = [
     description: `Probably has to do with internet connection issues nothing with your code.`,
     description2: `https://github.com/nextauthjs/next-auth/discussions/3186`,
   },
+  {
+    title: `When we use Prisma with Next.Js, why do we need the prismadb.ts file located in lib folder?`,
+    description: `Next uses hot-reload while developing, meaning that if you just did export const prisma = new PrismaClient() you would have a new prisma client with every hot reload. This would throw a warning saying "too many active prisma instances" and would cause bugs in development. To prevent this, we check if we are in development mode, and if we are, we assign new PrismaClient() to global. This is because global is not affected by hot reload. So that way we have that util to either use direct new PrismaClient() if we are in production, or use global/globalThis/globalForPrisma if we are in development.`,
+    description2: `https://discord.com/channels/1079557715497595013/1079557715497595016/1127972310834483300`,
+  },
 ];
